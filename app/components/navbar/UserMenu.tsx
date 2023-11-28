d9cebae724a2963885ce8f5cb0341cef9ed20b75
mjacoud <m.jacoud01@gmail.com>
2023-11-28 20:16:01 -0300

    fixed userMenu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,15 +7,10 @@ import { MenuItem } from './MenuItem'
 import { useRegisterModal } from '@/app/hooks/useRegisterModal'
 import { useLoginModal } from '@/app/hooks/useLoginModal'
 import { signOut } from 'next-auth/react'
-import { SafeUser } from '@/app/types'
 import { usePublishEventModal } from '@/app/hooks/usePublishEventModal'
 import { useRouter } from 'next/navigation'
 
-interface UserMenuProps {
-  currentUser?: SafeUser | null
-}
-
-export const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+export const UserMenu = () => {
   const router = useRouter()
   const registerModal = useRegisterModal()
   const loginModal = useLoginModal()
@@ -27,19 +22,19 @@ export const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     setIsOpen(value => !value)
   }, [])
 
-  const onPublishEvent = useCallback(() => {
+  /*   const onPublishEvent = useCallback(() => {
     if (!currentUser) {
       return loginModal.onOpen()
     }
 
     return publishEventModal.onOpen()
   }, [currentUser, loginModal, publishEventModal])
-
+ */
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div
-          onClick={onPublishEvent}
+          /* onClick={onPublishEvent} */
           className="
           trasition
           hidden
@@ -72,14 +67,15 @@ export const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         >
           <AiOutlineMenu />
           <div className="hidden md:block">
-            <Avatar src={currentUser?.image} />
+            {/* <Avatar src={currentUser?.image} /> */}
           </div>
         </div>
       </div>
       {isOpen && (
         <div className="absolute right-0 top-12 w-[40vw] overflow-hidden rounded-xl bg-white text-sm shadow-md md:w-3/4">
           <div className="flex cursor-pointer flex-col">
-            {currentUser ? (
+            {
+              /* currentUser ? (
               <>
                 <MenuItem onClick={loginModal.onOpen} label="Meus Eventos" />
                 <MenuItem
@@ -93,12 +89,11 @@ export const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 <hr />
                 <MenuItem onClick={() => signOut()} label="Deslogar" />
               </>
-            ) : (
-              <>
+            ) : */ <>
                 <MenuItem onClick={loginModal.onOpen} label="Login" />
                 <MenuItem onClick={registerModal.onOpen} label="Inscrever-se" />
               </>
-            )}
+            }
           </div>
         </div>
       )}
